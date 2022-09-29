7bec5ab0b38c28f9af1561880bba3df7b69e62b1
fadyafadilah <114629375+fadyafadilah@users.noreply.github.com>
2022-09-29 11:18:07 +0700

    Add files via upload

---
+++ aplikasiBiodata.py
@@ -0,0 +1,26 @@
+# Aplikasi Biodata Buatan Oleh M. Fadya Amar Fadilah
+
+#Input Nama
+nama = input(" Masukan Nama anda !")
+# Print / output Nama
+print("Nama anda adalah : ",nama)
+
+#Input NIM 
+NIM = input("Masukan NIM anda !")
+# Print / output NIM
+print("NIM anda adalah : ",NIM)
+
+#Input Kelas
+Kelas = input("Masukan Kelas anda !")
+# Print / output Kelas 
+print("Kelas anda adalah : ",Kelas)
+
+#Input Prodi
+Prodi = input("Masukan Prodi anda !")
+# Print / output Prodi
+print("Prodi anda adalah : ",Prodi)
+
+#Input Tanggal Lahir
+Tanggallahir = input("Masukan Tanggal Lahir anda !")
+# Print / output Tanggal Lahir
+print("Tanggal Lahir anda adalah : ",Tanggallahir)
